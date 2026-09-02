e6893b903a79208cbca9da1357366bbd98043f9c
Ayush Kumar <ayush.kumar2.cs28@iilm.edu>
2026-09-02 16:06:03 +0530

    Time: 104 ms (59.82%), Space: 120.1 MB (60.73%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        int n = candyType.size();
-        set<int> st;
-        for(int i = 0; i < n; i++) {
-            st.insert(candyType[i]);
-        }
-        int m = st.size();
-        int answer = min(m, n / 2);
-        return answer;
+        unordered_set<int> st(candyType.begin(), candyType.end());
+        return min(st.size(), candyType.size() / 2);
     }
 };
